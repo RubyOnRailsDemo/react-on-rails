9b4738328ec8104263af77b2892d5282cc84bae2
Lokesh G <77878864+lokeshg-fsd@users.noreply.github.com>
2021-11-26 21:04:38 +0530

    Update hello_react.jsx

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -10,7 +10,16 @@ const Hello = (props) => (
   <>
     <div>Hello Team!</div>
     {props.data.map((item, index) => (
-      <div key={index}>Hello {item.name} Welcome! <br />   Visit Again {item.name}. <br /> </div> 
+      <div key={index}>Hello {item.name} Welcome! <br />  
+      <div style="width: 80px;height: 42px;  display: flex;">
+  <div style="display: flex; flex-direction: row; align-items: center; 
+  justify-content: space-evenly; gap: 2px; width: 10px; height: 10px;
+   border-radius: 50%; transform: rotate(45deg); overflow: hidden; background-color: rgb(249, 180, 158);">
+  <span style="width: 2px; height: 10px; background-color: rgb(242, 105, 62);"></span>
+  <span style="width: 2px; height: 10px; background-color: rgb(242, 105, 62);"></span>
+  <span style="width: 2px; height: 10px; background-color: rgb(242, 105, 62);"></span></div>
+  <div style="width: 60px; font-size: 10px;padding-left: 6px;">No shows /<br> Same-day<br> cancellations</div>
+</div>  Visit Again {item.name}. <br /> </div> 
       
     ))}
     <div>Thank you all!</div>
